25b3f0130b8bd4b39b9ff17c739dd6d45119c749
Leonela <loliventuro@gmail.com>
2020-05-15 23:07:27 -0300

    segundo cambio

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ let boton = document.getElementById("icono");
 let enlaces = document.getElementById("enlaces");
 let contador = 0;
 
-boton.addEventListener("click", function() {
+boton.addEventListener("click", function(e) {
+    e.preventDefault();
     if (contador == 0) {
         enlaces.className = ("enlaces dos")
         contador = 1;
     } else {
         enlaces.classList.remove("dos")
-        enlaces.className = ("enlases uno")
+        enlaces.className = ("enlaces uno")
         contador = 0;
     }
 })
